2d6bbc3cea215ae529d680d3aaf6f4ff1e093839
Marcelo Lopes <marcelo@MacBook-Pro-de-Marcelo.local>
2024-02-07 20:17:18 -0300

    deploy to dokku

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-today = `date +%m-%d-%Y`
+# today = `date +%m-%d-%Y`
 
-git add . && git commit -m "Deploy to dokku in ~> ${today}" && git push -f dokku master && git push -f origin master
+# git add . && git commit -m "Deploy to dokku in ~> ${today}" && git push -f dokku master && git push -f origin master
+
